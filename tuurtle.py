3482d8f8a6a137ee962586e73e44d2ebcc305b73
Villy Volnka <36362446+VilliVonka@users.noreply.github.com>
2020-02-07 23:39:57 +0300

    Update tuurtle.py

--- tuurtle.py
+++ tuurtle.py
@@ -1,68 +1,18 @@
 import turtle
 
+#настраиваем всё окно
+window = turtle.Screen()
+window.bgcolor("orange")
+window.screensize(900,500)
+#подбираем размер оптимального рабочего окна
+turtle.setup(width=1100, height=600, startx=None, starty=None)
+
+
+pen = turtle.Turtle()
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+window.mainloop()
 
 
 
